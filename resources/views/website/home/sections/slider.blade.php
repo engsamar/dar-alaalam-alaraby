abde0dd665f613552139bf03e8c6b6a0a7957b8a
Hadeermagdy206 <hadeermagdy027@gmail.com>
2024-12-22 21:22:20 +0200

    edit header top + header center + navbar + main-slider

--- resources/views/website/home/sections/slider.blade.php
+++ resources/views/website/home/sections/slider.blade.php
@@ -1,25 +1,29 @@
 @if(! empty($result['sliders']) && count($result['sliders']) > 0 )
     <div class="main-banner-area">
-        <div class="container">
-            <div class="row align-items-center owl-carousel banner-slides">
+            <div class="align-items-center owl-carousel banner-slides">
                 @foreach ($result['sliders'] as $item )
-                    <div class="col-md-12">
-                        <div class="main-banner-content">
-                            <span class="sub-title">{{ $item->sub_title }}</span>
-                            <h1>{{ $item->title }}</h1>
-                            <a href="{{ $item->link ?? route('website.store.index',['locale'=> $locale]) }}" class="link-h">
-                                <span>@lang('titles.shopNow')</span>
-                            </a>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="banner-content">
+                                <span class="sub-title">{{ $item->sub_title }}</span>
+                                <h1>{{ $item->title }}</h1>
+                                <a href="{{ $item->link ?? route('website.store.index',['locale'=> $locale]) }}" class="link-h">
+                                    <span>@lang('titles.shopNow')</span>
+                                </a>
+                            </div>
                         </div>
 
-                        <div class="main-banner-image">
-                            <img src="{{ imagePath($item->image) ?? asset('assets/img/slide-2.webp') }}" alt="banner-image">
+                        <div class="col-md-6">
+                            <div class="banner-image">
+                                <img src="{{ imagePath($item->image) ?? asset('assets/img/slide-2.webp') }}" alt="banner-image">
+                            </div>
                         </div>
                     </div>
+                </div>
                 @endforeach
 
             </div>
-        </div>
     </div>
 @endif
 
--- resources/views/website/layouts/navbar.blade.php
+++ resources/views/website/layouts/navbar.blade.php
@@ -4,56 +4,54 @@
     $parameters = \Request::route()->parameters();
 @endphp
 <div class="navbar-area">
-    <div class="patoi-responsive-nav">
+    <div class="patoi-nav">
         <div class="container">
-            <div class="patoi-responsive-menu">
-                <div class="logo">
-                    <a href="{{ route('website.home',['locale'=> $locale]) }}"><img src="{{ isset($setting->logo) ? $setting->logoPath($locale)  : '' }}" alt="logo"></a>
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="nav-inner">
+                        <div class="menu-left">
+                            <div class="btn-cat">
+                                <i class="fal fa-grid"></i>
+                                <span>@lang('titles.categories') </span>
+                            </div>
+                            <div class="catogories-list">
+                                <ul>
+                                    @foreach ($topCategories as $cat)
+                                    <li>
+                                        <a href="{{ route('website.store.index',['locale'=> $locale ]) }}">
+                                            <i class="fal fa-grid"></i>
+                                            {{ $cat->title }}
+                                        </a>
+                                    </li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                        </div>
+
+                        <nav class="navbar navbar-expand-md navbar-light">
+                            <div class="collapse navbar-collapse mean-menu">
+                                <ul class="navbar-nav">
+                                    <li class="nav-item">
+                                        <a href="{{ route('website.home',['locale'=> $locale]) }}"
+                                            class="nav-link {{ Route::is('website.home') ? 'active' : '' }}">@lang('titles.home')
+                                        </a>
+                                    </li>
+
+                                    <li class="nav-item"><a href="{{ route('website.store.index',['locale'=> $locale]) }}" class="nav-link {{ Route::is('website.store.index') && request()->special == false  && request()->best_seller == false ? 'active' : '' }}">@lang('titles.store')</a></li>
+                                    <li class="nav-item"><a href="{{ route('website.store.index',['locale'=> $locale,'special' => true]) }}" class="nav-link {{ Route::is('website.store.index') && request()->special  ? 'active' : '' }}">@lang('titles.BestSellers')</a></li>
+                                    <li class="nav-item"><a href="{{ route('website.store.index',['locale'=> $locale,'best_seller' => true]) }}" class="nav-link {{ Route::is('website.store.index') && request()->best_seller ? 'active' : '' }}">@lang('titles.OurBestSellers')</a></li>
+                                    <li class="nav-item"><a href="{{ route('website.brands.index',['locale'=> $locale]) }}" class="nav-link {{ Route::is('website.brands.index') ? 'active' : '' }}">@lang('titles.brands')</a></li>
+                                    <li class="nav-item"><a href="{{ route('website.about.show',['locale'=> $locale]) }}" class="nav-link {{ Route::is('website.about.show') ? 'active' : '' }}">@lang('titles.about')</a></li>
+                                    <li class="nav-item"><a href="{{ route('website.contact.show',['locale'=> $locale]) }}" class="nav-link {{ Route::is('website.contact.show') ? 'active' : '' }}">@lang('titles.contact')</a></li>
+                                </ul>
+                            </div>
+                        </nav>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-    <div class="patoi-nav">
-        <div class="container">
-            <nav class="navbar navbar-expand-md navbar-light">
-                <div class="collapse navbar-collapse mean-menu">
-                    <ul class="navbar-nav">
-                        <li class="nav-item">
-                            <a href="{{ route('website.home',['locale'=> $locale]) }}"
-                                class="nav-link {{ Route::is('website.home') ? 'active' : '' }}">@lang('titles.home')
-                            </a>
-                        </li>
-
-                        <li class="nav-item"><a href="{{ route('website.store.index',['locale'=> $locale]) }}" class="nav-link {{ Route::is('website.store.index') && request()->special == false  && request()->best_seller == false ? 'active' : '' }}">@lang('titles.store')</a></li>
-                        <li class="nav-item"><a href="{{ route('website.store.index',['locale'=> $locale,'special' => true]) }}" class="nav-link {{ Route::is('website.store.index') && request()->special  ? 'active' : '' }}">@lang('titles.BestSellers')</a></li>
-                        <li class="nav-item"><a href="{{ route('website.store.index',['locale'=> $locale,'best_seller' => true]) }}" class="nav-link {{ Route::is('website.store.index') && request()->best_seller ? 'active' : '' }}">@lang('titles.OurBestSellers')</a></li>
-                        <li class="nav-item"><a href="{{ route('website.brands.index',['locale'=> $locale]) }}" class="nav-link {{ Route::is('website.brands.index') ? 'active' : '' }}">@lang('titles.brands')</a></li>
-                        <li class="nav-item"><a href="{{ route('website.about.show',['locale'=> $locale]) }}" class="nav-link {{ Route::is('website.about.show') ? 'active' : '' }}">@lang('titles.about')</a></li>
-                        <li class="nav-item"><a href="{{ route('website.contact.show',['locale'=> $locale]) }}" class="nav-link {{ Route::is('website.contact.show') ? 'active' : '' }}">@lang('titles.contact')</a></li>
-                    </ul>
-                    <div class="others-option">
-                        <div class="d-flex align-items-center">
-                            <ul>
-                                @if (! empty($locales))
-                                    <li>
-                                        <a href="{{ route(\Illuminate\Support\Facades\Route::currentRouteName(), $parameters) }}">
-                                            <i class="fa-solid fa-globe language-switch" style="cursor: pointer;margin-left: 0;font-size: 24px;"></i>
-                                        </a>
-                                    </li>
-                                @endif
-                                <li>
-                                    <a href="{{ auth()->guest() ? route('website.login',['locale'=> $locale]) : route('website.auth.profile.home',['locale'=> $locale]) }}">
-                                        <i class='bx bx-user-circle'></i>
-                                    </a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </nav>
-        </div>
-    </div>
-    <div class="others-option-for-responsive">
+    <!--<div class="others-option-for-responsive">
         <div class="container">
             <div class="dot-menu">
                 <div class="inner">
@@ -85,43 +83,5 @@
                 </div>
             </div>
         </div>
-    </div>
-</div>
-
-<div class="header-mobile">
-    <div class="item">
-        <a href="{{ route('website.store.index',['locale'=> $locale]) }}" class="search-icon">
-            <i class='bx bx-search'></i>
-            <span>@lang('titles.Search')</span>
-        </a>
-    </div>
-    <div class="item">
-        <a href="{{ route('website.store.index',['locale'=> $locale]) }}" class="nav-link {{ Route::is('website.store.index') ? 'active' : '' }}">
-            <i class='bx bx-store'></i>
-            <span>
-                @lang('titles.store')
-            </span>
-        </a>
-    </div>
-    <div class="item">
-        <a href="{{ route('website.home',['locale'=> $locale]) }}"
-            class="nav-link {{ Route::is('website.home') ? 'active' : '' }}">
-            <i class='bx bx-home'></i>
-            <span>@lang('titles.home')</span>
-        </a>
-    </div>
-    <div class="item">
-        <a href="{{ route('website.carts.index', ['locale' => $locale]) }}">
-            <i class='bx bx-cart'></i>
-            <span>
-                @lang('titles.Cart')
-            </span>
-        </a>
-    </div>
-    <div class="item">
-        <a href="{{ auth()->guest() ? route('website.login',['locale'=> $locale]) : route('website.auth.profile.home',['locale'=> $locale]) }}">
-            <i class='bx bx-user'></i>
-            <span>@lang('titles.profile')</span>
-        </a>
-    </div>
+    </div>-->
 </div>
